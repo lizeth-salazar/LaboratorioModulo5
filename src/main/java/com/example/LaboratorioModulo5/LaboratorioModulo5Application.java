221df2bf07bf5b1c9e0c981b5c91e9faf9a730cd
lizeth-salazar <lizeth.ss@gmail.com>
2018-07-06 23:04:12 -0400

    GeneratedCode from spring.io

---
+++ src/main/java/com/example/LaboratorioModulo5/LaboratorioModulo5Application.java
@@ -0,0 +1,12 @@
+package com.example.LaboratorioModulo5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratorioModulo5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratorioModulo5Application.class, args);
+	}
+}
---
+++ src/test/java/com/example/LaboratorioModulo5/LaboratorioModulo5ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.LaboratorioModulo5;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaboratorioModulo5ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
